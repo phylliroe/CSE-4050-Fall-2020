e2c022b455cc118030a48bd837ef2b0fd0fd863b
andy <andy@localhost.localdomain>
2020-08-25 14:49:14 -0700

    comments

--- js/script.js
+++ js/script.js
@@ -1,19 +1,35 @@
+// Counts the number of times the button is clicked
 var clicks = 0;
 
+// When the window loads, print a message to the web console
+// and call the clock() function
 window.onload = function() {
     console.log("Printing to the console!");
+    
+    // Call clock() function 
     clock();
+    
+    // Refresh clock every second
+    setInterval(clock, 1000);
 }
 
+// Count the number of times the button is clicked
 function button_click() {
     let button_results = document.getElementById("button_result_div");
     button_results.innerHTML = "<p>" + clicks + " button clicks</p>"
+    
+    // Increment click count by 1
     clicks++;
 }
 
+// Create a real time clock
 function clock() {
+    // div element where clock is placed
     let time_div = document.getElementById("box_text");
+    
+    // Get time
     let time = new Date().toLocaleTimeString();
+    
+    // Set element text
     time_div.innerHTML = time;
-    setInterval(clock, 1000);
 }
