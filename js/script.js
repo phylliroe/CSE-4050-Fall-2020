75c041e461010b4587d5218d3b8b49dec7ab92a2
andy <andy@localhost.localdomain>
2020-08-26 15:17:03 -0700

    more javascript

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+/*
+    Name: Andrew Loop-Perez
+    ID: 006198799
+    Course: CSE 4050
+    Assignment: JavaScript
+    Term: Fall 2020
+    Filename: script.js
+*/
+
 // Counts the number of times the button is clicked
 var clicks = 0;
 
@@ -20,9 +29,32 @@ function button_click() {
     
     // Increment click count by 1
     clicks++;
+
+    // Disable the button after a certain number of clicks
+    const max_clicks = 15;
+    
+    if (clicks > max_clicks) {
+        alert("You're clicking too much! I'm turning turning the button off!");
+        let the_button = document.getElementById("btn");
+
+        the_button.innerHTML = "DISABLED";
+        the_button.disabled = true;
+    }
 }
 
-// Create a real time clock
+// Add the values in the input boxes and display them in the "Results" text area
+// Called when the "Submit" button is clicked
+function add_input() {
+    // Convert input values to numbers and add them.
+    let a = Number(document.getElementById("val_a").value);
+    let b = Number(document.getElementById("val_b").value);
+    let result = a + b;
+ 
+    // Display the result
+    document.getElementById("add_result").value = result;
+}
+
+// Create a real time clock and display it on the page
 function clock() {
     // div element where clock is placed
     let time_div = document.getElementById("box_text");
