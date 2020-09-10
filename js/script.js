2287e1b9fa933cb098fddd843b657a03756b717a
andy <andy@localhost.localdomain>
2020-09-10 13:43:36 -0700

    added JSON

--- js/script.js
+++ js/script.js
@@ -10,12 +10,26 @@
 // Counts the number of times the button is clicked
 var clicks = 0;
 
+var j = {"first":"Andy", "last":"Loop", "next":6};
+var jar = [{"one":1, "two":2}, {"three":3}, {"four":4, "five":5}];
+var j2 = JSON.stringify(j);
+var jstring = '{ "employees" : [' +
+'{ "firstName":"John" , "lastName":"Doe" },' +
+'{ "firstName":"Anna" , "lastName":"Smith" },' +
+'{ "firstName":"Peter" , "lastName":"Jones" } ]}'; 
+
+var jr = JSON.parse(jstring);
+
 // When the window loads, print a message to the web console
 // and call the clock() function
 window.onload = function() {
     // Print to the console
     console.log("Printing to the console!");
-    
+
+    // Json
+    working_with_json();
+
+
     // Add the elements in an array to the webpage
     let my_array = ["This", "is", "an", "array"];
     let array_div =document.getElementById("array_div");
@@ -79,4 +93,77 @@ function clock() {
     
     // Display the clock on the HTML
     time_div.innerHTML = time;
+}
+
+// Function for demonstrating JSON
+function working_with_json() {
+    let json_1_div = document.getElementById("json_1");
+    let json_2_div = document.getElementById("json_2");
+    let json_3_div = document.getElementById("json_3");
+
+    // A simple JSON object
+    let json_basic = {"fname":"Andrew", "lname":"Loop-Perez", "id":"006189799"};
+
+    // A JSON array
+    let json_array = [{"Name": "John Doe", "Age":25}, {"Name": "Jane Doe", "Age": 34}, {"Name": "Doe Doe"}];
+    
+    // JSON object where the values are also JSON objects
+    let json_array_num = [
+        {
+            "Some Numbers": {
+                "One":1,
+                "Two": 2
+            },
+            "Another Number": {
+                "Three": 3
+            },
+            "More Numbers": {
+                "Four": 4,
+                "Five":5
+            },
+            "Six": 6,
+            "Seven":7
+        }
+    ];
+
+    // A string that will be parsed into a JSON object
+    let json_text = '{"text":"This is a string", "number":9}';
+
+    // Print the JSON objects to the console
+    console.log("Printing the JSON objects: ");
+    console.log(json_basic);
+    console.log(json_array);
+    console.log(json_array_num);
+
+    console.log(" ");
+
+    // Print individual elemens of the JSON objects to the console
+    console.log("Accessing elements: ");
+    console.log(json_basic["fname"]);
+    console.log(json_array[0]);
+    console.log(json_array[1]["Age"])
+    console.log(json_array_num[0]["Some Numbers"]);
+    console.log(json_array_num[0]["Some Numbers"]["Two"]);
+
+    // Stringify a JSON obkect and add it to the web page
+    let json_stringified = JSON.stringify(json_basic);
+    json_1_div.innerHTML += "<p>" + json_stringified + "</p>";
+
+    // Loop through the JSON array and add the "Name" values to the web page
+    let p = "<p> The Names Are: ";
+    for (let i = 0; i < json_array.length; i++) {
+        p += json_array[i]["Name"] + ", ";
+    }
+    p += "</p>"
+    json_2_div.innerHTML = p;
+
+
+    console.log(" ");
+
+    // Parse the string into a JSON object and print it to the console 
+    json_3_div.innerHTML += "<p>" + json_text + "</p>";
+
+    let json_parsed = JSON.parse(json_text);
+    console.log("Parsed JSON object: ");
+    console.log(json_parsed);
 }
