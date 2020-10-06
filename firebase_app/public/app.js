4376eab426e7e77692d50c1c5722061fca235636
andy <andy@localhost.localdomain>
2020-10-05 18:27:45 -0700

    more work

--- firebase_app/public/app.js
+++ firebase_app/public/app.js
@@ -4,71 +4,75 @@ console.log("The script is working!");
 var count = 1;
 
 function button_click() {
-    let list = document.getElementById("list");
-    
-    let new_div = document.createElement("div");
-    let text_box_val = document.getElementById("item_text").value;
-    
-    let add_button = document.createElement("button");
-    let delete_button = document.createElement("button");
-    let linebreak = document.createElement("br");
+    let input = document.getElementById("item_text").value;
+    if (!input || !input.trim().length) {
+        alert("No input");
+    }
+    else {
+        let list = document.getElementById("list");
+        
+        let new_div = document.createElement("div");
+        let text_box_val = document.getElementById("item_text").value;
+        
+        let add_button = document.createElement("button");
+        let delete_button = document.createElement("button");
+        let linebreak = document.createElement("br");
 
-    list.appendChild(new_div);
-    //list.appendChild(linebreak);
-    //list.appendChild(add_button);
-    //list.appendChild(delete_button);
-    //list.appendChild(linebreak);
-
-
-    //new_div.appendChild(delete_button);
-
-    let item_id = "item" + count;
-    new_div.setAttribute("id", item_id);
-    //new_div.setAttribute("class", "list_item");
-    new_div.className = "list_item";
-       
-    new_div.innerHTML = "<p class='item_name'>" + text_box_val + "</p>";
-    console.log(new_div.id);
+        list.appendChild(new_div);
+        //list.appendChild(linebreak);
+        //list.appendChild(add_button);
+        //list.appendChild(delete_button);
+        //list.appendChild(linebreak);
 
 
-    test(new_div.id);
+        //new_div.appendChild(delete_button);
 
-    list.appendChild(linebreak);
+        let item_id = "item" + count;
+        new_div.setAttribute("id", item_id);
+        //new_div.setAttribute("class", "list_item");
+        new_div.className = "list_item";
+        
+        new_div.innerHTML = "<p class='item_name'>" + text_box_val + "</p>";
 
-    //list.appendChild(add_button);
-    //list.appendChild(delete_button);
-    //new_div.appendChild(linebreak);
+        test(new_div.id);
 
-    //add_button.textContent = "ADD";
-    //add_button.setAttribute("class", "sub_btn");
-    //add_button.className = "sub_btn"
-    //add_button.classList.add("sub_btn");
+        list.appendChild(linebreak);
 
+        //list.appendChild(add_button);
+        //list.appendChild(delete_button);
+        //new_div.appendChild(linebreak);
 
-    //delete_button.textContent = "REMOVE";
-    //delete_button.classList.add("sub_btn");
-
-    //delete_button.onclick = delete_click;
-
-    //let id = "div" + count;
-    
-    //new_div.setAttribute("id", "div1");
-    //new_div.setAttribute("class", "list_item");
-    //new_div.className = "list_item";
-
-    //new_div.innerHTML = "<p>NEW</p>";
+        //add_button.textContent = "ADD";
+        //add_button.setAttribute("class", "sub_btn");
+        //add_button.className = "sub_btn"
+        //add_button.classList.add("sub_btn");
 
 
-    //let delete_btn_id = "delete" + count;
-    //delete_button.setAttribute("id", delete_btn_id);
-    //list.appendChild(new_div);
-    //list.appendChild(add_button);
+        //delete_button.textContent = "REMOVE";
+        //delete_button.classList.add("sub_btn");
 
-    //new_div.innerHTML += "<br>";
-    //console.log(id);
-    
+        //delete_button.onclick = delete_click;
+
+        //let id = "div" + count;
+        
+        //new_div.setAttribute("id", "div1");
+        //new_div.setAttribute("class", "list_item");
+        //new_div.className = "list_item";
+
+        //new_div.innerHTML = "<p>NEW</p>";
+
+
+        //let delete_btn_id = "delete" + count;
+        //delete_button.setAttribute("id", delete_btn_id);
+        //list.appendChild(new_div);
+        //list.appendChild(add_button);
+
+        //new_div.innerHTML += "<br>";
+        //console.log(id);
+        count++;
+    }
+
     clear_text();
-    count++;
 }
 
 function clear_text() {
@@ -89,8 +93,8 @@ function test(elem) {
     let delete_btn_id = "delete" + count;
     delete_button.setAttribute("id", delete_btn_id);
 
-    
-    console.log(delete_button.id);
+    delete_button.innerHTML = '<i class="fa fa-trash"></i>';
+
     e.appendChild(delete_button);
 }
 
@@ -106,16 +110,12 @@ list div
 Remove the new div and the following br
 */
 function delete_click() {
-    console.log(this.id);
-    //this.remove();
-    //console.log(this.parentNode.parentNode.id);
-    let a = this.parentNode.nextSibling;
-    console.log(a);
-    this.parentNode.parentNode.removeChild(a);
+    let next = this.parentNode.nextSibling;
+    this.parentNode.parentNode.removeChild(next);
     this.parentNode.parentNode.removeChild(this.parentNode);
 }
 
+// Remove a given element
 function delete_item(id) {
-    //node.parentNode.removeChild(node);
     document.getElementById("list").removeChild(node);
 }
