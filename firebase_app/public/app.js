1fb51d9b8a969ece7007a70d6b679623c0151f17
andy <andy@localhost.localdomain>
2020-10-10 17:08:15 -0700

    cleaned up current code

--- firebase_app/public/app.js
+++ firebase_app/public/app.js
@@ -1,69 +1,71 @@
 //console.log("test");
 console.log("The script is working!");
 
+// Counter variable used for assigning list item and button IDs
+var count = 1;
+
+// Sign user in with Google
 const auth = firebase.auth();
 const sign_in_button = document.getElementById("sign_in");
 const provider = new firebase.auth.GoogleAuthProvider();
-
-//sign_in_button.onclick = () => auth.signInWithPopup(provider);
-
-var count = 1;
 
 function google_auth() {
     auth.signInWithPopup(provider);
 }
 
+// Add a new list item when the button is clicked (or the ENTER key is pressed)
 function button_click() {
     let input = document.getElementById("item_text").value;
   
+    // Print message if input is blank or only consists of white space
     if (!input || !input.trim().length) {
         alert("No input");
     }
     else {
-        let list = document.getElementById("list");
-        
-        let new_div = document.createElement("div");
+        let list = document.getElementById("list");        
         let text_box_val = document.getElementById("item_text").value;
         
+        // Create new elements 
+        let new_div = document.createElement("div");
         let add_button = document.createElement("button");
         let delete_button = document.createElement("button");
         let linebreak = document.createElement("br");
-
         let p = document.createElement("p");
 
+        // Append new list item
         list.appendChild(new_div);
         
         //new_div.appendChild(delete_button);
 
+        // Set item div ID and class
         let item_id = "item" + count;
         new_div.setAttribute("id", item_id);
-        //new_div.setAttribute("class", "list_item");
         new_div.className = "list_item";
         
-        //new_div.innerHTML = "<p class='item_name'>" + text_box_val + "</p>";
 
-        //test(new_div.id);
-
+        // Set list item text
         p.className = "item_name";
         p.innerText = text_box_val;
-
         new_div.appendChild(p);
 
+        // Add buttons to item div
         create_add_button(new_div.id);
         create_delete_button(new_div.id);
-
-        //list.appendChild(linebreak);
         
         count++;
     }
 
+    // Clear input box text
     clear_text();
 }
 
+// Clear input box
 function clear_text() {
     document.getElementById("item_text").value = "";
 }
 
+// Create an add button and add it to the list div
+// Button does not do anything yet
 function create_add_button(elem) {
     let e = document.getElementById(elem);
     let add_button = document.createElement("button");
@@ -72,18 +74,14 @@ function create_add_button(elem) {
     add_button.setAttribute("id", add_button_id);
     add_button.innerHTML = '<i class="fa fa-plus"></i>';
     add_button.setAttribute("title", "Add Subtask");
-
     e.appendChild(add_button);
 
 }
 
+// Create delete button
 function create_delete_button(elem) {
     let e = document.getElementById(elem);
-    //let br = document.createElement("br");
-    //e.appendChild(document.createElement("br"));
-
     let delete_button = document.createElement("button");
-    //delete_button.textContent = "REMOVE";
     delete_button.classList.add("del_btn");
     delete_button.onclick = delete_click;
 
@@ -97,24 +95,11 @@ function create_delete_button(elem) {
     e.appendChild(delete_button);
 }
 
-/*
-list div
-    new div 1
-        delete button 1
-    br
-    new div 2
-        delete button 2
-    br
-
-Remove the new div and the following br
-*/
+// Delete item from list
+// Called on delete button click
 function delete_click() {
-    let next = this.parentNode.nextSibling;
-    //this.parentNode.parentNode.removeChild(next);
+    // Button is a child of the item div, which is a child of the list div
+    // Call the removeChild function for the list div and give it the current item div
     this.parentNode.parentNode.removeChild(this.parentNode);
 }
 
-// Remove a given element
-function delete_item(id) {
-    document.getElementById("list").removeChild(node);
-}
