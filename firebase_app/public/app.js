dc1fbcf7f6b1a7a9b83abfe19c3c61484cea9670
andy <andy@localhost.localdomain>
2020-10-25 13:08:24 -0700

    firestore

--- firebase_app/public/app.js
+++ firebase_app/public/app.js
@@ -6,11 +6,110 @@ var count = 1;
 
 // Sign user in with Google
 const auth = firebase.auth();
+const add_button = document.getElementById("add_btn");
 const sign_in_button = document.getElementById("sign_in");
 const provider = new firebase.auth.GoogleAuthProvider();
 
+// Firestore
+const fs = firebase.firestore();
+
+auth.onAuthStateChanged(user => {
+    if (user) {
+        console.log(user.uid);
+        console.log(user.displayName);
+
+        document.getElementById("user").hidden = false;
+        document.getElementById("user_name").innerHTML = "Hello " + user.displayName;
+
+        document.getElementById("list").innerHTML = "";
+    }
+    else {
+        document.getElementById("user").hidden = true;
+        document.getElementById("user_name").innerHTML = "";
+    }
+});
+
+auth.onAuthStateChanged(user => {
+    if(user) {
+        const uid = user.uid;
+        console.log(uid);
+
+        let user_doc = fs.collection("lists").doc(uid);
+
+        // Check if document for current signed in user exists
+        user_doc.get()
+            .then((docSnapshot) => {
+                if (docSnapshot.exists) {
+                    console.log("User doc exists!");
+                } 
+                else {
+                    console.log("doc does not exists");
+                    console.log("Creating new user document");
+
+                    // Create user document
+                    user_doc.set({
+                        name: user.displayName,
+                        id: user.uid
+                    });
+                }
+            });
+        /*
+        user_doc.get()
+            .then((doc) => {
+                if (doc.get("items") != null) {
+                    console.log("has items");
+                    console.log(doc.get("items")["one"]);
+
+                    if ("two" in doc.get("items")) {
+                        console.log("there's three");
+                    }
+                    else {console.log("no three");}
+
+                    let m = doc.get("items");
+                    console.log(m);
+                }
+            });
+        */
+
+        add_button.onclick = () => {
+            let newest = "item" + count;
+            button_click();
+        
+            console.log(newest);
+        
+            let d = document.getElementById(newest);
+            console.log(d.textContent);
+
+            //let children = document.getElementById("list").getElementsByTagName("div");
+            //console.log(children.length);
+
+            //get_items();
+        }
+    }
+    else {
+        add_button.onclick = () => {
+            button_click();
+            console.log("not signed in");
+        }
+    }
+});
+
 function google_auth() {
     auth.signInWithPopup(provider);
+}
+
+function get_items() {
+    let c = document.getElementById("list").getElementsByTagName("div");
+    console.log(c.length);
+
+    //let j = {};
+    //for (let i = 0; i < c.length; i++) {
+        //console.log(c[i].textContent);
+    //    let p = c[i].textContent
+        //let ps = c[i].getElementsByTagName("p");
+        //console.log(ps[0].textContent);
+    //    j[]
+    //}
 }
 
 // Add a new list item when the button is clicked (or the ENTER key is pressed)
@@ -57,6 +156,8 @@ function button_click() {
 
     // Clear input box text
     clear_text();
+
+    //console.log(p.innerHTML);
 }
 
 // Clear input box
